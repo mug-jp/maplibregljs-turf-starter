01e9c980d6d4a08a9675d0303abab2e082c7a512
dayjournal <yasunorikirimoto@gmail.com>
2019-01-19 08:53:45 +0900

    :tada: InitialCommit
    - :tada: InitialCommit

---
+++ README.md
@@ -0,0 +1,134 @@
+# turfjs-starter
+
+![README02](./img/README02.png)
+
+![README03](./img/README03.png)
+
+Start Turf.js easily.  
+- [Turf.js v5.1.6](http://turfjs.org/)  
+- [MapboxGLJS v0.50.0](https://www.mapbox.com/mapbox-gl-js/api/)  
+- [webpack v4.20.2](https://webpack.js.org)  
+- node v10.0.0
+- npm v6.5.0
+
+<br>
+
+## Usage
+
+![README04](./img/README04.png)
+
+<br>
+
+Install package
+
+```
+
+npm install
+
+```
+
+<br>
+
+build
+
+```
+
+npm run build
+
+```
+
+<br>
+
+dev
+
+```
+
+npm run dev
+
+```
+
+---
+
+<br>
+<br>
+
+![README01](./img/README01.gif)
+
+<br>
+
+## License
+MIT
+
+Copyright (c) 2019 Yasunori Kirimoto
+
+<br>
+
+---
+
+<br>
+
+### Japanese
+
+<br>
+
+# Turf.js スターター
+
+![README02](./img/README02.png)
+
+![README03](./img/README03.png)
+
+Turf.jsを手軽に始める
+- [Turf.js v5.1.6](http://turfjs.org/)  
+- [MapboxGLJS v0.50.0](https://www.mapbox.com/mapbox-gl-js/api/) 
+- [webpack v4.20.2](https://webpack.js.org)  
+- node v10.0.0
+- npm v6.5.0
+
+<br>
+
+##  使用方法
+
+![README04](./img/README04.png)
+
+<br>
+
+パッケージインストール
+
+```
+
+npm install
+
+```
+
+<br>
+
+ビルド
+
+```
+
+npm run build
+
+```
+
+<br>
+
+開発
+
+```
+npm run dev
+
+```
+
+<br>
+<br>
+
+![README01](./img/README01.gif)
+
+<br>
+
+## ライセンス
+MIT
+
+Copyright (c) 2019 Yasunori Kirimoto
+
+<br>
---
+++ _resouce/js/script.js
@@ -0,0 +1,85 @@
+
+// MIERUNE MONO読み込み
+var map = new mapboxgl.Map({
+    container: "map",
+    style: {
+        version: 8,
+        sources: {
+            m_mono: {
+                type: "raster",
+                tiles: ["https://tile.mierune.co.jp/mierune_mono/{z}/{x}/{y}.png"],
+                tileSize: 256
+            }
+        },
+        layers: [{
+            id: "m_mono",
+            type: "raster",
+            source: "m_mono",
+            minzoom: 0,
+            maxzoom: 18
+        }]
+    },
+    center: [139.770, 35.676],
+    zoom: 13
+});
+
+
+map.on("load", function () {
+
+    // 元ポイントを取得
+    var features = turf.featureCollection([
+        turf.point([139.7594, 35.6865]),
+        turf.point([139.7692, 35.6665]),
+        turf.point([139.7812, 35.6849])
+    ]);
+
+    // 元ポイント表示
+    map.addSource("FeaturesPoint", {
+        type: "geojson",
+        data: features
+    });
+    map.addLayer({
+        id: "FeaturesPoint",
+        type: "circle",
+        source: "FeaturesPoint",
+        layout: {},
+        paint: {
+            "circle-pitch-alignment": "map",
+            "circle-stroke-color": "#1253A4",
+            "circle-stroke-width": 5,
+            "circle-stroke-opacity": 0.8,
+            "circle-color": "#1253A4",
+            "circle-radius": 5,
+            "circle-opacity": 0.5
+        }
+    });
+
+
+    // センターポイントを取得
+    var center = turf.center(features);
+
+    // センターポイント表示
+    map.addSource("CenterPoint", {
+        type: "geojson",
+        data: center
+    });
+    map.addLayer({
+        id: "CenterPoint",
+        type: "circle",
+        source: "CenterPoint",
+        layout: {},
+        paint: {
+            "circle-pitch-alignment": "map",
+            "circle-stroke-color": "#8DCF3F",
+            "circle-stroke-width": 10,
+            "circle-stroke-opacity": 0.8,
+            "circle-color": "#8DCF3F",
+            "circle-radius": 10,
+            "circle-opacity": 0.5
+        }
+    });
+
+});
+
+// コントロール関係表示
+map.addControl(new mapboxgl.NavigationControl());
